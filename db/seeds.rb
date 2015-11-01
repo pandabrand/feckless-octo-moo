2cc83f7fb799e0d7903b755068b1458d93fc5ebd
pandabrand <pandabrand@gmail.com>
2015-10-31 19:17:55 -0500

    updated rake file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #   name = shell.ask("What's your name?")
 #   shell.say name
 #
-email     = shell.ask "Which email do you want use for logging into admin?"
-password  = shell.ask "Tell me the password to use:", :echo => false
+email     = "darksidetaco@gmail.com"
+password  = "gert134027"
 
 shell.say ""
 
-account = Account.new(:email => email, :name => "Foo", :surname => "Bar", :password => password, :password_confirmation => password, :role => "admin")
+account = Account.new(:email => email, :name => "Taco", :surname => "Admin", :password => password, :password_confirmation => password, :role => "admin")
 
 if account.valid?
   account.save
