3848730159c4e58aaa29dd557412bca4f07f25c3
pandabrand <pandabrand@gmail.com>
2015-11-25 15:49:49 -0600

    added date verification, next add time verification

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,10 @@
 module Darksidetwo
   class App
     module HomeHelper
-      def lift_gate_helper
+      def lift_gate_helper(dates, compareTime)
+        return false if dates.nil?
+        dates_arr = dates.split ","
+        return dates_arr.any? {|date| compareTime == Date.parse(date)}
       end
     end
 
--- spec/app/helpers/home_helper_spec.rb
+++ spec/app/helpers/home_helper_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe "Darksidetwo::App::HomeHelper" do
   before { helpers.extend Darksidetwo::App::HomeHelper }
   subject { helpers }
     it "should return false when nil" do
-      expect(subject.lift_gate_helper).to be_false
+      expect(subject.lift_gate_helper nil, Time.now).to be_falsy
     end
-#   it "should return nil" do
-# 	expect(subject.foo).to be_nil
-#   end
+
+    it "should return false when today is not found" do
+      expect(subject.lift_gate_helper "2015-10-14,2015-12-23", Time.now.to_date).to be false
+    end
 end
